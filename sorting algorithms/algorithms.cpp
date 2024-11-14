136913b20b13473597dcb1882b34529af905a77b
JamesFral <145146199+JamesFral@users.noreply.github.com>
2024-11-14 16:48:26 +0000

    bug fixing complete

--- sorting algorithms/algorithms.cpp
+++ sorting algorithms/algorithms.cpp
@@ -1,15 +1,19 @@
 #include "algorithms.hpp"
 #include <array>
+#include <iostream>
 
 void bubbleSort(int n,int arr[] ) {
     bool swap = true;
+    std::cout << "made swap var\n";
     while (swap) {
+        std::cout << ":3\n";
         swap = false;
         for (int i = 0; i < n; i++) {
-            if (arr[i], arr[i+1]) {
+            if (arr[i] > arr[i+1]) {
                 int tempData = arr[i+1];
                 arr[i+1] = arr[i];
                 arr[i] = tempData;
+                swap = true;
             }
         }
     }
--- sorting algorithms/main.cpp
+++ sorting algorithms/main.cpp
@@ -1,7 +1,10 @@
-
-
+#include "algorithms.cpp"
 
 int main() {
-
+    int Bubble5[] = {5,2,8,9,4};
+    bubbleSort(5,Bubble5);
+    for (int i = 0; i <= 5; i++) {
+        std::cout << Bubble5[i] <<'\n';
+    }
     return 0;
 }
